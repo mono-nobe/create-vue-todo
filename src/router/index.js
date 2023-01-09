c7f804cf4bf16cbd252d8790b1d172c8383ab7f2
mono-nobe <ryoya.goemono@gmail.com>
2023-01-09 19:45:51 +0900

    不要なファイルやコードを削除

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import TodoView from "../views/TodoView.vue";
 
 const router = createRouter({
@@ -7,21 +6,8 @@ const router = createRouter({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/todo",
-      name: "todo",
+      name: "todos",
       component: TodoView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
     },
   ],
 });
